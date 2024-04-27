31985b18f3407fc84f06e335f7a269647e2aaf03
Alex Pareto <alex.pareto@gmail.com>
2024-04-27 12:29:46 -0700

    add function generation and preview on the website

--- victor-frontend/src/routes/Test/index.tsx
+++ victor-frontend/src/routes/Test/index.tsx
@@ -1,22 +1,31 @@
-import { AppShell, Burger, Button } from "@mantine/core";
+import { AppShell, Burger, Button, Input } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import styled from "@emotion/styled";
 import { useState } from "react";
 import axios from "axios";
 import config from "@/config";
+import { Program } from "@/utils/clientTypes";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { docco } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const TestElement = styled.div`
-  color: blue;
+const Header = styled.h1`
+  font-weight: bold;
 `;
 
 export const Test: React.FC = () => {
   const [opened, { toggle }] = useDisclosure();
 
-  const [data, setData] = useState<{ name: string | null }[]>([]);
+  const [program, setProgram] = useState<Program | null>(null);
+  const [promptInput, setPromptInput] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
-  const loadData = async () => {
-    const result = await axios.get(`${config.backendHost}/api/test-model`);
-    setData(result.data);
+  const generateProgram = async () => {
+    setLoading(true);
+    const result = await axios.post(`${config.backendHost}/api/programs`, {
+      prompt: promptInput,
+    });
+    setProgram(result.data.program);
+    setLoading(false);
   };
 
   return (
@@ -38,12 +47,42 @@ export const Test: React.FC = () => {
         <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
 
         <AppShell.Main>
-          <TestElement>
-            {data.map(({ name }) => (
-              <p>{name}</p>
-            ))}
-          </TestElement>
-          <Button onClick={loadData}>Load data</Button>
+          <Input
+            placeholder="Enter your input here"
+            value={promptInput}
+            onChange={(e) => setPromptInput(e.target.value)}
+          />
+          <Button loading={loading} onClick={generateProgram}>
+            Generate
+          </Button>
+          <div>
+            {program && (
+              <div>
+                <Header>{program.name}</Header>
+                {program.versions.map((version) => {
+                  return (
+                    <div style={{ maxWidth: 800, fontSize: 12 }}>
+                      <h3>
+                        Version {version.id} - Fitness: {version.fitness}
+                      </h3>
+                      <p>Body:</p>
+                      <SyntaxHighlighter
+                        wrapLines={true}
+                        language="javascript"
+                        style={docco}
+                        wrapLongLines={true}
+                      >
+                        {`${version.signature} {\n${version.body
+                          .split("\n")
+                          .map((line) => `    ${line}`)
+                          .join("\n")}\n}`}
+                      </SyntaxHighlighter>
+                    </div>
+                  );
+                })}
+              </div>
+            )}
+          </div>
         </AppShell.Main>
       </AppShell>
     </>
